cf2627b24a0cf56ec1ce6a96155055a469188ea5
dongwei77 <303492649@qq.com>
2017-09-18 16:57:34 +0800

    完成登录页面

--- public/js/common.js
+++ public/js/common.js
@@ -6,3 +6,33 @@
 	$('.navs ul').prev('a').on('click', function () {
 		$(this).next().slideToggle();
 	});
+
+	/*退出登录*/
+	$('#logoutBtn').on('click', function () {
+		$.ajax({
+			url : '/api/logout',
+			type: 'post',
+			success:function(data){
+				if(data.code == 200){
+					location.href = '/login';
+				}
+
+			}
+		})
+	})
+
+	//检测用户是否登录
+	var flag = $.cookie('PHPSESSID');
+	if(!flag){
+		/*如果flag不存在  跳转登录页面*/
+		location.href = '/main/login';
+	}
+
+	//设置用户头像信息
+    var loginInfo = $.cookie('loginInfo');
+	loginInfo = loginInfo && JSON.parse(loginInfo);
+	//设置用户头像信息
+	$('.aside .profile .avatar img').attr('src',loginInfo.tc_avatar);
+	$('.aside .profile h4').html(loginInfo.tc_name);
+
+
