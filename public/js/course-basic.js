63b4839f582b385902ec2c0a76ae6847129d772c
dongwei77 <303492649@qq.com>
2017-09-25 20:15:20 +0800

    完成课程编辑及添加页面数据渲染

--- public/js/course-basic.js
+++ public/js/course-basic.js
@@ -1,7 +1,7 @@
 /**
  * Created by Administrator on 2017/9/25.
  */
-define(['jquery','template','util'], function ($,template,util) {
+define(['jquery','template','util','ckeditor'], function ($,template,util,ckeditor) {
     //左侧菜单栏
     util.setMenu('/course/add');
     //获取课程id
@@ -21,6 +21,32 @@ define(['jquery','template','util'], function ($,template,util) {
            }
             var html = template('basicTpl',data.result);
             $('#basicInfo').html(html)
+
+
+            //处理二级分类的下拉联动
+            $('#firstType').on('change', function () {
+               var pid = $(this).val();
+                //根据一级分类的ID查询二级分类的数据
+                $.ajax({
+                    type:'get',
+                    url : '/api/category/child',
+                    data :{cg_id : pid},
+                    dataType : 'json',
+                    success : function (data) {
+                        console.log(data);
+                        //拼接二级分类的下拉选项
+                        var tpl = '<option value="">请选择二级菜单</option>{{each list}}<option value="{{$value.cg_id}}">{{$value.cg_name}}</option>{{/each}}';
+                        var html =template.render(tpl,{list:data.result});
+                        $('#secondType').html(html);
+
+                    }
+                })
+            })
+
+            //处理富文本
+            CKEDITOR.replace('editor');
         }
     })
+
+
 })
