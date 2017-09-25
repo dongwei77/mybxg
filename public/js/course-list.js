62cafba9ef47a0f4891493a4ec64f83ed3d7d6db
dongwei77 <303492649@qq.com>
2017-09-25 12:02:58 +0800

    完成课程列表数据渲染

--- public/js/course-list.js
+++ public/js/course-list.js
@@ -2,5 +2,18 @@
  * Created by Administrator on 2017/9/25.
  */
 define(['jquery','template','util'], function ($,template,util) {
-    util.setMenu(location.pathname)
+    util.setMenu(location.pathname);
+    //动态渲染数据
+    $.ajax({
+        type : 'get',
+        url : '/api/course',
+        dataType : 'json',
+        success : function (data) {
+            var html = template('courseTpl',data);
+
+
+            console.log(data);
+            $('#courseInfo').html(html);
+        }
+    })
 })
